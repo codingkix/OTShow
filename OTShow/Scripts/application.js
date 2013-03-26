31b7b49caf38138bb7f697dc7dd267b781f61fc5
coder <codekix@gmail.com>
2013-03-26 15:33:44 -0700

    load google map
    add google map
    initial layout

--- OTShow/Scripts/application.js
+++ OTShow/Scripts/application.js
@@ -1,3 +1,26 @@
 ﻿var GlobalSetting = {
-    feedUrl : 'http://feeds-na.otcorp.opentable.com/reservations/created/current.json'
+    feedUrl: 'http://feeds-na.otcorp.opentable.com/reservations/created/current.json',
+    googleAPIKey: 'AIzaSyCKe9-BfEAwyaR2Qb7XIRkq4FccIf4HOIM'
 }
+
+function initialGoogleMap() {
+    var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+}
+
+function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=" + GlobalSetting.googleAPIKey + "&sensor=true&callback=initialGoogleMap";
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
+$(document).ready(function(){
+   
+})
